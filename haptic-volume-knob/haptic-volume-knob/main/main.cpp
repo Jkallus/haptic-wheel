4cc14a530b811c9f7210400a05a55c47fa5a786a
Josh Kallus <joshk.kallus3@gmail.com>
2023-03-10 17:12:22 -0500

    Changed algorithm for detents to not rapidly flip current due to angle noise. Angle noise is measured to be .07 degrees.

--- haptic-volume-knob/haptic-volume-knob/main/main.cpp
+++ haptic-volume-knob/haptic-volume-knob/main/main.cpp
@@ -8,16 +8,12 @@
 
 #include <SimpleFOC.h>
 
-static const char *TAG = "example";
-
 #define A_h 17 
 #define A_l 16
 #define B_h 26
 #define B_l 25
 #define C_h 22
 #define C_l 21
-
-const float pi = 3.141592;
 
 // BLDC motor & driver instance
 BLDCMotor motor = BLDCMotor(7);
@@ -26,18 +22,17 @@ BLDCDriver6PWM driver = BLDCDriver6PWM(A_h, A_l, B_h, B_l, C_h, C_l);
 // magnetic sensor instance - SPI
 MagneticSensorSPI sensor = MagneticSensorSPI(AS5048_SPI, 5);
 
-float ma_per_degree = 85;
-float damper_constant = 0;
-float detent_width = 7.2;
-// instantiate the commander
-Commander command = Commander(Serial);
+float ma_per_degree = 85; // K constant
+float detent_width = 10; // Width between detents in angles
+float last_current = 0.0;
+float last_angle = 0.0;
+int count = 0;
+
+Commander command = Commander(Serial); // instantiate the commander
 void doTarget(char* cmd) { command.scalar(&ma_per_degree, cmd); }
-void doDamper(char* cmd) { command.scalar(&damper_constant, cmd); }
 void doWidth(char* cmd) {command.scalar(&detent_width, cmd); }
 void doMotor(char* cmd) { command.motor(&motor, cmd); }
-
 int i = 0;
-
 TaskHandle_t motor_task_handle = NULL;
 
 void setup_motor()
@@ -57,17 +52,15 @@ void setup_motor()
 	motor.current_limit = 1.0;
 	motor.torque_controller = TorqueControlType::voltage;
 	motor.controller = MotionControlType::torque; // set motion control loop to be used
-
 	
 	Serial.begin(115200); // use monitoring with serial 
 	motor.useMonitoring(Serial);
-	motor.monitor_variables = _MON_VOLT_Q | _MON_VOLT_D | _MON_ANGLE | _MON_VEL;
+	motor.monitor_variables = _MON_ANGLE | _MON_VEL;
 	
 	motor.init();
 	motor.initFOC(); // align sensor and start FOC
 
 	command.add('T', doTarget, "K Constant"); // - if phase resistance defined
-	command.add('D', doDamper, "Damping Constant");
 	command.add('M', doMotor, "motor");
 	command.add('W', doWidth, "Detent Width");
 
@@ -76,54 +69,15 @@ void setup_motor()
 	_delay(1000);
 }
 
-void run_loop()
-{
-	// main FOC algorithm function
-	gpio_set_level(GPIO_NUM_27, 1);
-	motor.loopFOC();
-	float cycle_time = 0.0002522;
-	float angle_rad = sensor.getAngle();
-	float velocity = sensor.getVelocity() * (180 / pi);
-	float angle = angle_rad * (180 / pi);
-	float threshold = 180;
-	float spring_current = 0;
-	float damper_current = 0;
-
-	if (angle < threshold)
-	{
-		motor.move(0);
-	}
-	else
-	{
-		spring_current = (angle - threshold) * (ma_per_degree / 1000.0);
-		//damper_current = velocity * ((ma_per_degree / 1000.0) * cycle_time) / 2;
-		float total_current = spring_current; // + damper_current;
-		if (total_current > 1.0)
-		{
-			total_current = 1.0;
-		}
-		motor.move(total_current);
-	}  
-  
-	//motor.monitor();
-
-	// user communication
-	command.run();
-	gpio_set_level(GPIO_NUM_27, 0);
-}
-
 void run_loop_detent()
 {	
 	// main FOC algorithm function
 	gpio_set_level(GPIO_NUM_27, 1);
 	motor.loopFOC();
   
-	float cycle_time = 0.000300;
 	float angle_rad = sensor.getAngle();
-	float velocity = sensor.getVelocity() * (180 / pi);
-	float angle = angle_rad * (180 / pi);
+	float angle = angle_rad * (180 / PI);
 	float spring_current = 0;
-	float damper_current = 0;
 	float total_current = 0;
 
 	if (angle > 0)
@@ -131,39 +85,62 @@ void run_loop_detent()
 		float phase = fmod(angle, detent_width);
 		if (phase < detent_width / 2)
 		{
-			spring_current = phase * (ma_per_degree / 1000.0);
+			if (last_current < 0)
+			{
+				if (abs(phase - detent_width / 2) > 0.1)
+				{
+					spring_current = phase * (ma_per_degree / 1000.0);
+				}
+				else
+				{
+					spring_current = last_current;
+				}
+			}
+			else
+			{
+				spring_current = phase * (ma_per_degree / 1000.0);
+			}
 		}
 		else
 		{
-			float phase2 = fmod(phase, detent_width / 2.0);
-			spring_current = ((detent_width / 2) - phase2) * (ma_per_degree / 1000.0) * -1;
+			if (last_current > 0)
+			{
+				if ((abs(phase - detent_width / 2)) > 0.1)
+				{
+					float phase2 = fmod(phase, detent_width / 2);
+					spring_current = (detent_width / 2 - phase2) * (ma_per_degree / 1000.0) * -1;
+				}
+				else
+				{
+					spring_current = last_current;
+				}
+			}
+			else
+			{
+				float phase2 = fmod(phase, detent_width / 2);
+				spring_current = (detent_width / 2 - phase2) * (ma_per_degree / 1000.0) * -1;
+			}
 		}
 	}
-	else
-	{
-		float phase = fmod(angle, detent_width);
-		if (phase > (-detent_width / 2))
-		{ 
-			spring_current = phase * (ma_per_degree / 1000.0);
-		}
-		else
-		{
-			float phase2 = fmod(phase, detent_width / 2.0);
-			spring_current = ((detent_width / 2) + phase2) * (ma_per_degree / 1000.0);
-		}
-	}
-
-	///damper_current = velocity * damper_constant;
+	
 	total_current = spring_current;
 	total_current = constrain(total_current, -1.0, 1.0);
+	
+	float peak_current_threshold = (ma_per_degree / 1000.0) * (detent_width / 2) * 0.9;
+	bool at_snap_edge = abs(last_current) > peak_current_threshold && abs(total_current) > peak_current_threshold; // check that the transition that may be occuring is at the snap edge and not in the settling point between snaps
+	if (last_current > 0 && total_current < 0 && at_snap_edge) // CCW snap
+	{
+		Serial.println(--count);		
+	}
+	else if(last_current < 0 && total_current > 0 && at_snap_edge) // CW snap
+	{
+		Serial.println(++count);
+	}
+	last_current = total_current;
+	
 	motor.move(total_current);
-
-//	motor.monitor();
-//	if (i++ % 200 == 0) {
-//		String toprint = String(spring_current) + "\t" + String(damper_current) + "\t"  + String(angle) + "\t" + String(velocity) +  "\n";
-//		Serial.print(toprint);
-//	}
-  
+	
+	//Serial.println(angle);
 	command.run();
 	gpio_set_level(GPIO_NUM_27, 0);
 }
