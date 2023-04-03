31d2e2830d1f84d351eee08935e6fca1bb140947
Josh Kallus <joshk.kallus3@gmail.com>
2023-04-02 21:39:07 -0400

    Added volume monitoring and updating HID device

--- hid-volume-sync-test-netframework/Program.cs
+++ hid-volume-sync-test-netframework/Program.cs
@@ -1,10 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using HidLibrary;
-
+using NAudio;
+using NAudio.CoreAudioApi;
 
 namespace hid_volume_sync_test_netframework
 {
@@ -16,9 +14,16 @@ namespace hid_volume_sync_test_netframework
 
         private static HidDevice _device;
 
+        private static MMDeviceEnumerator enumerator = new MMDeviceEnumerator();
+        
+        private static MMDevice dev;
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+
+            dev = enumerator.GetDefaultAudioEndpoint(DataFlow.Render, Role.Multimedia);
+            dev.AudioEndpointVolume.OnVolumeNotification += AudioEndpointVolume_OnVolumeNotification;
 
             _device = HidDevices.Enumerate(VendorID, ProductID).FirstOrDefault();
 
@@ -35,7 +40,25 @@ namespace hid_volume_sync_test_netframework
             else
             {
                 Console.WriteLine("Could not find device");
+                Console.ReadLine();
             }
+        }
+
+        private static void AudioEndpointVolume_OnVolumeNotification(AudioVolumeNotificationData data)
+        {
+            byte percent = Convert.ToByte(data.MasterVolume * 100);
+            Console.WriteLine($"Volume updated to {data.MasterVolume}, Percent value: {percent}");
+            WriteData(percent);
+        }
+
+        private static byte GetCurrentVolume()
+        {
+            float volume = dev.AudioEndpointVolume.MasterVolumeLevelScalar;
+            if (volume > 1.0 || volume < 0.0)
+                throw new Exception("Volume out of range");
+            byte percent = Convert.ToByte(volume * 100);
+            Console.WriteLine($"Got current volume: {percent}");
+            return percent;
         }
 
         private static void OnReport(HidReport report)
@@ -46,15 +69,13 @@ namespace hid_volume_sync_test_netframework
             var reportData = report.Data;
 
             Console.WriteLine($"Report data: {reportData[0]:X2}");
-
-            WriteData(0x45);
-
             _device.ReadReport(OnReport);
         }
 
         private static void DeviceAttachedHandler()
         {
             Console.WriteLine("Device attached");
+            WriteData(GetCurrentVolume()); // Write initial volume when connected
             _device.ReadReport(OnReport);
         }
 
@@ -68,6 +89,16 @@ namespace hid_volume_sync_test_netframework
             var report = _device.CreateReport();
             report.ReportId = 1;
             report.Data = new byte[] { data };
+            Console.WriteLine($"Writing {data} to device");
+            _device.WriteReport(report);
+
+        }
+
+        private static void WriteData(byte[] data)
+        {
+            var report = _device.CreateReport();
+            report.ReportId = 1;
+            report.Data = data;
             _device.WriteReport(report);
         }
     }
